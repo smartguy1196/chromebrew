a80795ea440c29a24cdb50f37f7edbc3f72d79a4
Satadru Pramanik <satadru@gmail.com>
2022-09-23 17:40:51 -0400

    add vulkan updates

--- packages/vulkan_headers.rb
+++ packages/vulkan_headers.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vulkan_headers < Package
   description 'Vulkan header files'
   homepage 'https://github.com/KhronosGroup/Vulkan-Headers'
-  @_ver = '1.3.224'
+  @_ver = '1.3.229'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Vulkan_headers < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.224_armv7l/vulkan_headers-1.3.224-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.224_armv7l/vulkan_headers-1.3.224-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.224_i686/vulkan_headers-1.3.224-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.224_x86_64/vulkan_headers-1.3.224-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.229_armv7l/vulkan_headers-1.3.229-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.229_armv7l/vulkan_headers-1.3.229-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.229_i686/vulkan_headers-1.3.229-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_headers/1.3.229_x86_64/vulkan_headers-1.3.229-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '07d06412605ad77da5eb584ff0004ca8bd375facdec7f208cc8507d87574770a',
-     armv7l: '07d06412605ad77da5eb584ff0004ca8bd375facdec7f208cc8507d87574770a',
-       i686: '09b838eacafe97c1471f3da15a82fef46bbd895b443f50f0cefb080b4e513595',
-     x86_64: '20e3b90d93447e620d730bfe3f0f55d97cfd19134ed275b5672f5d962863118a'
+    aarch64: 'da4e70de5956a0fb42cabb2600a510da01d68100aae0a10ea03bceff25f740a0',
+     armv7l: 'da4e70de5956a0fb42cabb2600a510da01d68100aae0a10ea03bceff25f740a0',
+       i686: 'f96c726b742626f4fcdb8c63ecf7a43ded58cbeef331e7a293873524cb284146',
+     x86_64: 'a19456ce3200cfae66bc7ea573a4054d9a3da25a291a3cf88a27234c6de9b72e'
   })
 
   def self.build
@@ -31,10 +31,10 @@ class Vulkan_headers < Package
         #{CREW_CMAKE_OPTIONS} \
         .."
     end
-    system 'ninja -C builddir'
+    system 'mold -run samu -C builddir'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
 end
--- packages/vulkan_icd_loader.rb
+++ packages/vulkan_icd_loader.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vulkan_icd_loader < Package
   description 'Vulkan Installable Client Driver ICD Loader'
   homepage 'https://github.com/KhronosGroup/Vulkan-Loader'
-  @_ver = '1.3.224'
+  @_ver = '1.3.229'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Vulkan_icd_loader < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.224_armv7l/vulkan_icd_loader-1.3.224-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.224_armv7l/vulkan_icd_loader-1.3.224-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.224_i686/vulkan_icd_loader-1.3.224-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.224_x86_64/vulkan_icd_loader-1.3.224-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.229_armv7l/vulkan_icd_loader-1.3.229-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.229_armv7l/vulkan_icd_loader-1.3.229-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.229_i686/vulkan_icd_loader-1.3.229-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_icd_loader/1.3.229_x86_64/vulkan_icd_loader-1.3.229-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '15b9c5272c0a2a8adcafd5984da16738a3a7032479b38f9ce77a3b4ddaf187e5',
-     armv7l: '15b9c5272c0a2a8adcafd5984da16738a3a7032479b38f9ce77a3b4ddaf187e5',
-       i686: 'c0c0c5a6fed09d2515de58c8f2af31081133fe8bc292d445b7bf5ce8676a4239',
-     x86_64: '53fdaa92fb1e31410c6ebd50f1661f8b99b3a62a2f1bc33be123b2fff7a3f2da'
+    aarch64: '3be36f97ef53c0a3cac8c49a2ce56a2032164153e75c8c7d02c8a2f3ebda9235',
+     armv7l: '3be36f97ef53c0a3cac8c49a2ce56a2032164153e75c8c7d02c8a2f3ebda9235',
+       i686: '587c560fb00c00c5b00534108bd868accb5e5f92a0e55a4209b6ea7444da4e96',
+     x86_64: 'b8d152548a2240d956be41e237ee0ce134e5dbb829badf8ef104899cd31642bf'
   })
 
   depends_on 'libx11'
